244fb82eb00112f314c2ad5c18da5df7859ac0d3
Muhammad Usman Tahir <m.usman.tahir.336@gmail.com>
2021-10-15 00:52:00 +0500

    Update Rock,paper,scissors.py

--- Rock,paper,scissors.py
+++ Rock,paper,scissors.py
@@ -1,24 +1,15 @@
 import random
 chars = "RPS"
 def rps(User,computer):
-    if  User== "R" and computer== "P":
-        return "computer won!!"
-    elif User == "P" and computer == "R":
-        return "You won!!"
-    elif User == "P" and computer == "S":
-        return "computer won!!"
-    elif User == "S" and computer == "P":
-        return "You won!!"
-    elif User == "R" and computer == "S":
-        return "You won!!"
-    elif User == "S" and computer == "P":
-        return "computer won!!"
-    elif User not in chars:
+    if User not in chars:
         return "wrong input"
     elif User == computer:
         return "tie"
-run = True
+    return "You won" if (User== "S" and computer== "P") or (User == "P" and computer == "R") or (User == "R" and computer == "S") else "Computer Won"
+run = 3
 while run:
-    User = input("R for rock ,P for Paper,S for scissors : \n")
+    run -= 1
+    User = input("R for rock ,P for Paper,S for scissors : \nYour Choice: ").upper()
     computer = random.choice(chars)
+    print("Computer's choice",computer)
     print(rps(User,computer))
